7f720cf483f50062db4786593ade893a8a8691d4
estebonbon <estesarmiento1@gmail.com>
2024-01-18 20:48:13 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Write your JavaScript below this line
+var tempCelsius = 10;
+var toFahrenheit = tempCelsius * 1.8 + 38;
+
+console.log(
+  `Outside my friends house it's ${tempCelsius}°C which is ${toFahrenheit}°F`
+);
